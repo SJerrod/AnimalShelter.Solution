f73f5ca7383fc34d40ead8cd9946b95961f0e655
Jerrod Styrk <jstyrk@citadel.edu>
2021-01-22 08:38:10 -0800

    Create Models and constructors for Cat and Dog

---
+++ AnimalShelterApi/Models/Dog.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelterApi.Models
+{
+    public class Dog
+    {
+        public int DogId { get; set; }
+        public string DogName { get; set; }
+        public string DogGender { get; set; }
+        public int DogAge { get; set; }
+        public string DogBreed { get; set; }
+        public string DogTemperment { get; set; }
+    }
+}
