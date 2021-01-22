7cb6a3d3f9987db74c5a4c4d04dd23963c46da76
Jerrod Styrk <jstyrk@citadel.edu>
2021-01-22 11:30:28 -0800

    Add Routing to DogsController

--- AnimalShelterClient/Controllers/DogsController.cs
+++ AnimalShelterClient/Controllers/DogsController.cs
@@ -17,5 +17,42 @@ namespace AnimalShelterClient.Controllers
             var allDogs = Dog.GetDogs();
             return View(allDogs);
         }
+
+        public IActionResult Details(int id)
+        {
+            var thisDog = Dog.GetDetails(id);
+            return View(thisDog);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Dog dog)
+        {
+            Dog.Post(dog);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisDog = Dog.GetDetails(id);
+            return View(thisDog);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Dog dog)
+        {
+            Dog.Put(dog);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Dog.Delete(id);
+            return RedirectToAction("Index");
+        }
     }
 }
