391fcf4e3f7344e78dce46ea9482d42309377e88
Jerrod Styrk <jstyrk@citadel.edu>
2021-01-22 11:08:35 -0800

    Add CRUD functionality for Dog in APIHelper

--- AnimalShelterClient/Models/ApiHelper.cs
+++ AnimalShelterClient/Models/ApiHelper.cs
@@ -3,22 +3,48 @@ using RestSharp;
 
 namespace AnimalShelterClient.Models
 {
-  class ApiHelper
-  {
-    public static async Task<string> GetAll()
+    class ApiHelper
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
-      RestRequest request = new RestRequest($"dogs", Method.GET);
-      var response = await client.ExecuteTaskAsync(request);
-      return response.Content;
-    }
+        public static async Task<string> GetAll()
+        {
+            RestClient client = new RestClient("http://localhost:5000/api");
+            RestRequest request = new RestRequest($"dogs", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
 
-    public static async Task<string> Get(int id)
-    {
-      RestClient client = new RestClient("http://localhost:5000/api");
-      RestRequest request = new RestRequest($"dogs/{id}", Method.GET);
-      var response = await client.ExecuteTaskAsync(request);
-      return response.Content;
+        public static async Task<string> Get(int id)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api");
+            RestRequest request = new RestRequest($"dogs/{id}", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+
+        public static async Task Post(string newDog)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api");
+            RestRequest request = new RestRequest($"dogs", Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(newDog);
+            var response = await client.ExecuteTaskAsync(request);
+        }
+
+        public static async Task Put(int id, string newDog)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api");
+            RestRequest request = new RestRequest($"dogs/{id}", Method.PUT);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(newDog);
+            var response = await client.ExecuteTaskAsync(request);
+        }
+
+        public static async Task Delete(int id)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api");
+            RestRequest request = new RestRequest($"dogs/{id}", Method.DELETE);
+            request.AddHeader("Content-Type", "application/json");
+            var response = await client.ExecuteTaskAsync(request);
+        }
     }
-  }
 }
